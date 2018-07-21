cea226905d1cd23558ede0bdc3c1d0234c900f0d
Joshua Hudson <hudo1239@gmail.com>
2018-07-21 22:47:28 +0100

    In the middle of creating the single read

--- models/Post.php
+++ models/Post.php
@@ -44,4 +44,43 @@
 
             return $stmt;
         }
+
+        //Get Sinlge Post
+
+        public function read_single(){
+            $query = 'SELECT 
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+            FROM
+                ' . $this->table . ' p
+            LEFT JOIN
+                categories c ON p.category_id = c.id
+            WHERE
+                p.id = ? 
+            LIMIT 
+                0,1';
+
+            // Prepare statment
+            $stmt = $this->conn->prepare($query);
+
+            //Bind Id
+            $stmt->bindParam(1, $this->id);
+
+            //Execute
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            //Set propertise
+            $this->title = $row['title'];
+            $this->body = $row['body'];
+            $this->author = $row['author'];
+            $this->category_id = $row['category_id'];
+            $this->category_name = $row['category_name'];
+        }
     }
